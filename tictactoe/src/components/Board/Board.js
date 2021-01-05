5b0b9513b24c565a7be4a98b50624824429c429b
Sandrine <nunubabila@gmail.com>
2021-01-04 17:12:01 -0800

    cell

--- tictactoe/src/components/Board/Board.js
+++ tictactoe/src/components/Board/Board.js
@@ -1,37 +1,22 @@
+import React from 'react';
+import { Cell } from '../Cell/Cell';
 
 import './Board.css';
 
 // import and conver App.js then export
+// import cell buttons from cell component and duplicate
 export const Board = () => {
   return ( 
     <div id="board">
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
-          <button className="cell">
-              <span className="cell-content"></span>
-          </button>
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
+          <Cell />
       </div>
   );
 }
---
+++ tictactoe/src/components/Cell/Cell.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Cell.css';
+
+// import and conver App.js then export
+export const Cell = () => {
+  return ( 
+    
+    <button className="cell">
+              <span className="cell-content"></span>
+          </button>
+  );
+}
+
+
